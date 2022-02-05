0d63554944d17c8bc7f3d5e2c064c9387e779894
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-02-05 12:47:27 -0800

    Agregando funcionalidad reiniciar y cambiando codigo para respetar standard.

--- scripts/gato.js
+++ scripts/gato.js
@@ -2,28 +2,40 @@
 const $reiniciar = document.querySelector('.reiniciar')
 const $estatus = document.querySelector('.estatus')
 const $celdas = document.querySelectorAll('.celda')
-const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
+const $modal_container = document.getElementById('modal_container');
+const $close = document.getElementById('close');
 
 /*Variables de Juego para controlar el flujo*/
 let estaJugando = true
 let esXSiguiente = true
-let ganador = null
 
 /*Simbolos usados en el CSS::Before*/
 const simboloX = '╳'
 const simboloO = '◯'
 
 /*Funciones para cambiar el estado de la aplicacion*/
-const ejecutarReset = (evento) => {
-    console.log(evento)
-}
 
 /*Funciones para cerrar el pop-up*/
 const cerrarPopUp = (evento) => {
-    close.addEventListener('click', () => {
-        modal_container.classList.remove('show');
-    });
+    $modal_container.classList.remove('show');
+}
+
+const obtenerSimbolo = (letra) => letra === 'x' ? simboloX : simboloO
+
+const ganador = (letra) => {
+    estaJugando = false
+    $estatus.innerHTML = `${obtenerSimbolo(letra)} ha ganado. Felicidades!`
+}
+
+const ejecutarReset = () =>{
+    $estatus.innerHTML = `El siguiente es: ${simboloX}`
+    esXSiguiente = true
+    for (const celda of $celdas){
+        celda.classList.remove('x')
+        celda.classList.remove('o')
+    }
+    estaJugando = true
+
 }
 
 const clickEnCelda = (evento) => {
@@ -35,7 +47,7 @@ const clickEnCelda = (evento) => {
     //Validar si la clase ya contiene 'X' u 'O' para no agregar dos valores repetidos
     if(listaDeClases[2]=== 'x' || listaDeClases[2]=== 'o' ){
       //pop-up de error
-        modal_container.classList.add('show');
+        $modal_container.classList.add('show');
         return
     }else{
         //Agregar el valor del click a la clase basado en la variable booleana "esXSiguiente"
@@ -57,8 +69,6 @@ const clickEnCelda = (evento) => {
     }
 }
 
-const obtenerSimbolo = (letra) => letra === 'x' ? simboloX : simboloO
-
 const verificarEstadoDelJuego = () => {
     const arribaIzquierda = $celdas[0].classList[2]
     const arribaMedio = $celdas[1].classList[2]
@@ -75,53 +85,33 @@ const verificarEstadoDelJuego = () => {
     //En caso de que si, se asigna la variable ganadora y se asigna false a "estaJugando" ya que la ejecucion debe terminar.
     //verifica horizontales
     if(arribaIzquierda != null && arribaIzquierda === arribaMedio && arribaMedio === arribaDerecha){
-        estaJugando = false
-        ganador = arribaIzquierda
-        $estatus.innerHTML = `${obtenerSimbolo(ganador)} ha ganado. Felicidades!`
+        ganador(arribaIzquierda)
     }else if (medioIzquierda != null && medioIzquierda === medioMedio && medioMedio === medioDerecha){
-        estaJugando = false
-        ganador = medioIzquierda
-        $estatus.innerHTML = `${obtenerSimbolo(ganador)} ha ganado. Felicidades!`
+        ganador(medioIzquierda)
     }else if (abajoIzquierda != null && abajoIzquierda === abajoMedio && abajoMedio === abajoDerecha){
-        estaJugando = false
-        ganador = abajoIzquierda
-        $estatus.innerHTML = `${obtenerSimbolo(ganador)} ha ganado. Felicidades!`
+        ganador(abajoIzquierda)
     }
     //verifica verticales
     else if (arribaIzquierda != null && arribaIzquierda === medioIzquierda && medioIzquierda === abajoIzquierda){
-        estaJugando = false
-        ganador = arribaIzquierda
-        $estatus.innerHTML = `${obtenerSimbolo(ganador)} ha ganado. Felicidades!`
+        ganador(arribaIzquierda)
     } else if (arribaMedio != null && arribaMedio === medioMedio && medioMedio === abajoMedio){
-        estaJugando = false
-        ganador = arribaMedio
-        $estatus.innerHTML = `${obtenerSimbolo(ganador)} ha ganado. Felicidades!`
+        ganador(arribaMedio)
     }else if (arribaDerecha != null && arribaDerecha === medioDerecha && medioDerecha === abajoDerecha){
-        estaJugando = false
-        ganador = arribaDerecha
-        $estatus.innerHTML = `${obtenerSimbolo(ganador)} ha ganado. Felicidades!`
+        ganador(arribaDerecha)
     }
     //verifica horizontales
     else if (arribaIzquierda != null && arribaIzquierda === medioMedio && medioMedio === abajoDerecha){
-        estaJugando = false
-        ganador = arribaIzquierda
-        $estatus.innerHTML = `${obtenerSimbolo(ganador)} ha ganado. Felicidades!`
+        ganador(arribaIzquierda)
     }else if(arribaDerecha != null && arribaDerecha === medioMedio && medioMedio === abajoIzquierda){
-        estaJugando = false
-        ganador = arribaDerecha
-        $estatus.innerHTML = `${obtenerSimbolo(ganador)} ha ganado. Felicidades!`
+        ganador(arribaDerecha)
     }
-
-    //En progreso funcion para manejar el ganador y logica de demas formas de ganar.
-
-
 }
 
 /*Event Listeners para escuchar los cambios en el DOM*/
 $reiniciar.addEventListener('click', ejecutarReset)
 
 /* Evento para cerrar el pop-up */
-close.addEventListener('click', cerrarPopUp)
+$close.addEventListener('click', cerrarPopUp)
 
 //La sentencia sentencia for...of ejecuta un bloque de código para cada elemento de un objeto iterable, como lo son: String, Array, objetos similares a array
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Statements/for...of
